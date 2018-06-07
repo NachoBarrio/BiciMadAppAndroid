b7bc62b06b9a6af1f46b318cd9b881796c3e6c6a
naxetete89 <naxetete@hotmail.com>
2018-06-07 04:10:02 +0200

    - Clases POJO

---
+++ app/src/main/java/data/model/BiciMad.java
@@ -0,0 +1,76 @@
+package data.model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class BiciMad {
+
+    @SerializedName("code")
+    @Expose
+    private String code;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("whoAmI")
+    @Expose
+    private String whoAmI;
+    @SerializedName("version")
+    @Expose
+    private String version;
+    @SerializedName("time")
+    @Expose
+    private String time;
+    @SerializedName("data")
+    @Expose
+    private String data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWhoAmI() {
+        return whoAmI;
+    }
+
+    public void setWhoAmI(String whoAmI) {
+        this.whoAmI = whoAmI;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
